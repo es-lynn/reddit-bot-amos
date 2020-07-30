35181f403fd84655c707fdb383c590fd81c29495
Jaslyn S <7008316+aelesia@users.noreply.github.com>
2020-07-31 02:56:38 +0800

    CORS from ENV

--- handler.ts
+++ handler.ts
@@ -1,6 +1,7 @@
 import { LambdaAPI, LambdaConfig } from '@aelesia/commons/dist/src/aws/lambda/LambdaAPI'
 import * as lambda from './src/lambda/Lambda'
+import { Cfg } from './src/app/config/Cfg'
 
-LambdaConfig.whitelist = ['http://localhost:3080']
+LambdaConfig.whitelist = Cfg.CORS_WHITELIST
 
 export const _records = LambdaAPI(lambda.records)
--- src/app/config/Env.ts
+++ src/app/config/Env.ts
@@ -7,7 +7,7 @@ export function envAWSResource(resource: string): string {
   return `${Pkg.name}-${env('ENVIRONMENT')}-${env(resource)}`
 }
 
-export function env(env: string): string {
+export function env(env: string): any {
   const envvar = process.env[env]
   if (envvar == null || envvar === '') {
     if (process.env['NODE_ENV'] === 'test' || process.env['ENVIRONMENT'] === 'test') {
@@ -15,10 +15,16 @@ export function env(env: string): string {
     }
     throw new Err.UninitializedErr(`[Config.${env}] Not set in .env`)
   }
-  return envvar as string
+  if (
+    (envvar[0] === '{' && envvar[envvar.length - 1] === '}') ||
+    (envvar[0] === '[' && envvar[envvar.length - 1] === ']')
+  ) {
+    return JSON.parse(envvar)
+  }
+  return envvar
 }
 
-export function env_<T>() {
+function env_<T>() {
   return (key: keyof T): string => {
     const envvar = process.env[key as any]
     if (envvar == null || envvar === '') {
@@ -30,9 +36,20 @@ export function env_<T>() {
     return envvar as string
   }
 }
+export const Env = env_<EnvType>()
 
-export const Env = env_<{
-  ENVIRONMENT: string
+/**
+ * @experimental
+ * I'm not entirely convinced that parsing .env data as JSON is a good idea
+ * This is what Cfg is meant for. It reads the raw data from .env and parses it to a suitable format for usage
+ * Why this could be a potentially bad idea:
+ *   - Other platforms will be reading this as string while app reads it as JSON
+ *   - W̶i̶l̶l̶ ̶T̶E̶S̶T̶_̶E̶N̶V̶_̶V̶A̶R̶ ̶c̶a̶u̶s̶e̶ ̶i̶s̶s̶u̶e̶s̶ ̶i̶n̶ ̶c̶a̶s̶e̶s̶ ̶l̶i̶k̶e̶ ̶[̶]̶.̶l̶a̶s̶t̶(̶)̶ ̶i̶s̶ ̶n̶o̶t̶ ̶d̶e̶f̶i̶n̶e̶d̶
+ *     - False. EnvJSON is safer to use for testing
+ */
+export const EnvJSON: <K extends keyof EnvType>(key: K) => EnvType[K] = env
+type EnvType = {
+  ENVIRONMENT: 'test' | 'local' | 'develop' | 'staging' | 'production' | string
   SUBREDDIT: string
   AWSC_ACCESS_KEY_ID: string
   AWSC_SECRET_ACCESS_KEY: string
@@ -44,4 +61,5 @@ export const Env = env_<{
   O2A_CLIENT_ID: string
   O2A_SECRET: string
   O2A_PASSWORD: string
-}>()
+  CORS_WHITELIST: string[]
+}
