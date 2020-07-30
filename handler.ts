049dbf3f0c1f83dce868bc52044cdba823700a3c
Jaslyn S <7008316+aelesia@users.noreply.github.com>
2020-07-31 01:26:59 +0800

    Test localhost:3080

--- handler.ts
+++ handler.ts
@@ -1,4 +1,6 @@
 import { LambdaAPI, LambdaConfig } from '@aelesia/commons/dist/src/aws/lambda/LambdaAPI'
 import * as lambda from './src/lambda/Lambda'
 
+LambdaConfig.whitelist = ['http://localhost:3080']
+
 export const _records = LambdaAPI(lambda.records)
