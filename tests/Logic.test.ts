79ca21b2c533de3a76dc94c0c62f62444c5003bb
Jaslyn S <jaslyn_soh@tech.gov.sg>
2021-08-11 00:47:23 +0800

    Fix 'a yee' detection
    Currently it will trigger if it detects 'A' and 'yee' in the post, regardless of where they are. A sentence like "Ong Yee Kuan said something. A new hope." would trigger the detection. This fixes it by ensuring that 'A' & 'yee' must always be beside one another with only non alphabet characters separating them (eg. A.Yee or A_Yee)

--- tests/Logic.test.ts
+++ tests/Logic.test.ts
@@ -3,7 +3,7 @@ import { DateUtil } from '@aelesia/commons/dist/src/collections/util/DateUtil'
 import { Cfg } from '../src/app/config/Cfg'
 import { Time } from '@aelesia/commons'
 import { PostFactory } from './factories/Factories'
-import { Post } from '../src/db/model/Post'
+import { Post } from 'reddit-ts/src/types/models/Post.type'
 
 describe('Logic', () => {
   test('Title contains "Amos"', async () => {
@@ -214,20 +214,30 @@ describe('Logic', () => {
     ).toEqual(true)
   })
 
-  test('contains_amos_a_y_yee', () => {
-    expect(Logic.contains_amos_a_y_yee("You'll never be as useless and unsuccessful as a certain A.YEE")).toBeTruthy()
-    expect(Logic.contains_amos_a_y_yee('y_amos = y_amos += x_amos')).toBeTruthy()
+  test('contains__a_yee__amos_y__y_amos', () => {
     expect(
-      Logic.contains_amos_a_y_yee(
+      Logic.contains__a_yee__amos_y__y_amos("You'll never be as useless and unsuccessful as a certain A.YEE")
+    ).toBeTruthy()
+    expect(Logic.contains__a_yee__amos_y__y_amos('y_amos = y_amos += x_amos')).toBeTruthy()
+    expect(
+      Logic.contains__a_yee__amos_y__y_amos(
         'Is this what Amos Y- (ok I can hear the bot making angry noises) would have become if he had been given an elite education?'
       )
     ).toBeTruthy()
+
+    expect(Logic.contains__a_yee__amos_y__y_amos('a_yee')).toBeTruthy()
+    expect(Logic.contains__a_yee__amos_y__y_amos('a yee')).toBeTruthy()
+    expect(Logic.contains__a_yee__amos_y__y_amos('amos.y')).toBeTruthy()
+    expect(Logic.contains__a_yee__amos_y__y_amos('amos y')).toBeTruthy()
+
+    expect(Logic.contains__a_yee__amos_y__y_amos('A fun yee')).toBeFalsy()
+    expect(Logic.contains__a_yee__amos_y__y_amos('ayee')).toBeFalsy()
   })
 
   test('is not amos_yee thread', () => {
-    expect(Logic.contains_amos_a_y_yee('AY')).toBeFalsy()
-    expect(Logic.contains_amos_a_y_yee('A.Y')).toBeFalsy()
-    expect(Logic.contains_amos_a_y_yee('A*** Y**')).toBeFalsy()
+    expect(Logic.contains__a_yee__amos_y__y_amos('AY')).toBeFalsy()
+    expect(Logic.contains__a_yee__amos_y__y_amos('A.Y')).toBeFalsy()
+    expect(Logic.contains__a_yee__amos_y__y_amos('A*** Y**')).toBeFalsy()
     expect(
       Logic.is_amos_yee_comment({
         body:
@@ -235,5 +245,14 @@ describe('Logic', () => {
           "A Reddit post over Shaun Ho's case can be found here: NTU undergraduate admits to taking illicit videos of 335 women, including victims on campus\n"
       } as Post)
     ).toBeFalsy()
+    // https://www.reddit.com/r/singapore/comments/oubigz/zb_schools_1000_covid19_isolation_beds_in_local/h7199re/?context=3
+    expect(
+      Logic.is_amos_yee_comment({
+        body:
+          'Health Minister Ong Yee Kung revealed that the local hospital currently has 1,000 isolation beds for coronary disease patients, and the number of beds in the intensive care unit has increased to nearly 70 in the past two weeks.\n' +
+          '\n' +
+          '"The Jurong Fishing Port infected groups have turned multiple markets into infected groups and must be closed. It is also likely to spread widely in the community, especially affecting the elderly who often visit the market. A quarter of the elderly have always been If they have not been vaccinated yet, they will be at risk of serious illness if they are infected."\n'
+      } as any)
+    ).toEqual(false)
   })
 })
